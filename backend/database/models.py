5e4c1f67e23efe0086ddc73058712df5ba51fc5a
Matthias Roos <matthias.roos@freenet.de>
2023-11-12 16:04:58 +0100

    Add timezone support

--- backend/database/models.py
+++ backend/database/models.py
@@ -15,11 +15,11 @@ class Entry(Base):
     id = sqlalchemy.Column(
         sqlalchemy.dialects.postgresql.UUID(as_uuid=True), primary_key=True, index=True, unique=True
     )
-    start_time = sqlalchemy.Column(sqlalchemy.DateTime())
-    stop_time = sqlalchemy.Column(sqlalchemy.DateTime())
+    start_time = sqlalchemy.Column(sqlalchemy.TIMESTAMP(timezone=True))
+    stop_time = sqlalchemy.Column(sqlalchemy.TIMESTAMP(timezone=True))
     type = sqlalchemy.Column(sqlalchemy.String(10))
-    created_at = sqlalchemy.Column(sqlalchemy.DateTime(),
+    created_at = sqlalchemy.Column(sqlalchemy.TIMESTAMP(timezone=True),
                                    default=sqlalchemy.func.now())
-    modified_at = sqlalchemy.Column(sqlalchemy.DateTime(),
+    modified_at = sqlalchemy.Column(sqlalchemy.TIMESTAMP(timezone=True),
                                     default=sqlalchemy.func.now(),
                                     onupdate=sqlalchemy.func.now())
